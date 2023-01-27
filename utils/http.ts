ab01e2914e8007eef9a9f223f533c249c2359ac1
Nicolas Manzato <nicomanzato@gmail.com>
2023-01-26 23:49:45 -0300

    added http options back

--- utils/http.ts
+++ utils/http.ts
@@ -1,6 +1,10 @@
+const defaultOptions = {
+  headers: { 'Content-Type': 'application/json' },
+};
+
 export const http = {
   get: async <T>(url: string, options?: RequestInit): Promise<T> => {
-    const response = await fetch(url, options);
+    const response = await fetch(url, { ...defaultOptions, ...options });
     return response.json();
   },
 };
